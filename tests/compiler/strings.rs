8a3b998889e988ccf7f494b9c2a50df5c14460f9
Ross Hadden <rosshadden@gmail.com>
2026-06-22 15:05:21 -0400

    test: `in` for substrings

--- tests/compiler/strings.rs
+++ tests/compiler/strings.rs
@@ -24,3 +24,29 @@ fn string_ne_true() {
 fn string_ne_false() {
 	check(r#""foo" != "foo""#, "false");
 }
+
+#[test]
+fn string_in_found() {
+	check(r#""foo" in "foobar""#, "true");
+}
+
+#[test]
+fn string_in_not_found() {
+	check(r#""baz" in "foobar""#, "false");
+}
+
+#[test]
+fn string_in_exact_match() {
+	check(r#""foo" in "foo""#, "true");
+}
+
+#[test]
+fn string_in_empty_value() {
+	// empty string is always a substring
+	check(r#""" in "foo""#, "true");
+}
+
+#[test]
+fn string_in_type_mismatch_error() {
+	assert!(fail(r#"42 in "foo""#).contains("type mismatch"));
+}
